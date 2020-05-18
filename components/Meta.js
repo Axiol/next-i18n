1ac7ecae1e3734c4c4de5266e1752d627a94b748
Arnaud Delante <contact@arnaud.ninja>
2020-05-18 19:33:58 +0200

    ✨ Add a loading indecator between pages

--- components/Meta.js
+++ components/Meta.js
@@ -4,6 +4,8 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="/nprogress.css" />
     <title>LaraClothes</title>
   </Head>
 );
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import App from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 
 import Page from '../components/Page';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+}
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+
+Router.onRouteChangeError = () => {
+  NProgress.done();  
+}
 
 class MyApp extends App {
   render() {
